19fbb9f677058c1735b6e757adfa4fc417d86a22
Vojtěch Stehlík <stehlvo2@fit.cvut.cz>
2025-05-23 16:16:35 +0200

    Fix

--- src/main/kotlin/cz/cvut/fit/stehlvo2/routing/request/IdTokenExchangeRequest.kt
+++ src/main/kotlin/cz/cvut/fit/stehlvo2/routing/request/IdTokenExchangeRequest.kt
@@ -1,7 +1,9 @@
 package cz.cvut.fit.stehlvo2.routing.request
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class IdTokenExchangeRequest(
     @SerialName("id_token") val idToken: String,
 )
